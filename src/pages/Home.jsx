3f852372ba575292da0bb16e974184036685bcea
vincentt94 <vincent.h.thao@gmail.com>
2025-03-06 12:51:29 -0600

    updated about me section on the home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -11,7 +11,14 @@ export default function Home() {
                     <img src={profilePic} alt="My Profile" className="profile-pic mb-3" width="150px" />
                 </div>
                 <p className="text-center">
-                    Lets update this main page ! Need to update my about me section! 
+                   My name is Vincent Thao.  I am currently in a Coding Bootcamp through the University of Minnesota.  As an aspiring Software Developer,
+                   I am constantly learning and growing.  I am thoroughly enjoying this journey.  I have found these past few months very challenging,
+                   yet so rewarding at the same time.  Just a few months ago, I knew nothing about software development.  Now, I can build a single-page application (along with many other Full-Stack applciations)
+                   in just a week, which amazes me.  
+                </p>
+                <p className="text-center">
+                    In my free time I enjoy doing various activities.  My favorite pasttime would probably be going to the gym and lifting weights.  I have two wonderful children,
+                    a daughter and a son.   I am very excited for whatever endeavors the future may hold. 
                 </p>
             </div>
         </div>

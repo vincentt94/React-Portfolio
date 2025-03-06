8a61f221038459f190f783b751b630ca37b71b24
vincentt94 <vincent.h.thao@gmail.com>
2025-03-06 12:02:13 -0600

    added feature branch

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -11,12 +11,7 @@ export default function Home() {
                     <img src={profilePic} alt="My Profile" className="profile-pic mb-3" width="150px" />
                 </div>
                 <p className="text-center">
-                    My name is Vincent.  I'm currently in a Software Development Bootcamp through the Universiy of Minnesota.
-                    I find coding very interesting to say the least.  I have a two year old daughter and a one year old son. 
-                    When I have free time I enjoy going to the gym.  I find it very soothing and relaxing and it's not a tedious task for me.
-                    I wouldn't consider it a hobby but rather a part of my lifestyle.  I'm not too particular with my diet and because of that
-                    I do indulge in eating whatever I feel like.   I'm enjoying my time learning the process of coding.  This page 
-                    will contain many kinks that will be fixed in future updates!
+                    Lets update this main page ! Need to update my about me section! 
                 </p>
             </div>
         </div>

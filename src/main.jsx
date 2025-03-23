77b1872458363d9a617f69decf4b2f28b29d50ac
vincentt94 <vincent.h.thao@gmail.com>
2025-03-22 23:56:35 -0500

    fixing build / deployment issues

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,5 @@
 // import { StrictMode } from 'react'
 // import { createRoot } from 'react-dom/client'
-import './app.css';
 import App from './App.jsx';
 import ReactDOM from 'react-dom/client';
 
